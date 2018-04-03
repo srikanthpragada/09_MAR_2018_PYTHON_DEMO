06d295e36828df9b21c31975e5b19aa713eb4ab5
srikanthpragada <srikanthpragada@gmail.com>
2018-04-03 20:59:58 +0530

    3-APR

--- files/directory_list.py
+++ files/directory_list.py
@@ -1,10 +1,24 @@
 # open file for writing in text mode
+import re
+
 phones = dict()
 with open(r"e:\classroom\python\mar9\phones.txt", "rt") as f:
     for line in f.readlines():
-        parts = line.split(',')
+        parts = line.split(",")
+
         if len(parts) < 2:
             continue
+
+        name = parts[0]
+        mobile = parts[1].strip('\n')
+        # print(name,mobile)
+        # validate name
+        if not re.fullmatch(r'[a-zA-Z ]+', name):
+            continue;
+        # validate mobile number
+        if not re.fullmatch(r'\d{10}',mobile):
+            continue;
+
         phones[parts[0]] = parts[1].strip('\n')
 
 for name, value in sorted(phones.items()):
